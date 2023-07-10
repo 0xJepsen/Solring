ec4631be4e423afb08eda7fd463eab442ef8c058
Waylon Jepsen <waylonjepsen1@gmail.com>
2023-07-09 22:13:09 -0600

    😤

--- src/Solring.sol
+++ src/Solring.sol
@@ -1,44 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-
-// Ring Buffer in solidity for story blockIDs
-contract SolringControl {
-    // Size of buffer is 10 
-    uint256[10] public arr;
-
-    // Set blockID at index blockID % 10
-    // Since blocks increase in a linear fassion 
-    // we can use the modulus operator to always set the index
-    // this will overwrite the oldest blockID in the buffer if buffer is full
-    function setNumber(uint256 blockID) payable public {
-        arr[(blockID % arr.length)] = blockID;
-    }
-
-    // Gets the blockID at index blockID % 10
-    // Will return a valid blockID even for overwritten blocks
-    function getNumber(uint256 blockID) public view returns (uint256) {
-        return arr[(blockID % arr.length)];
-    }
-}
-
-
 // Ring Buffer in solidity for story blockIDs
 contract Solring {
     // Size of buffer is 10 
-    uint256[10] public arr;
+    bytes32[10] public arr;
 
-    // Set blockID at index blockID % 10
+    // Set blockId at index blockId % 10
     // Since blocks increase in a linear fassion 
     // we can use the modulus operator to always set the index
-    // this will overwrite the oldest blockID in the buffer if buffer is full
-    function setNumber(uint256 blockID) payable public {
-        arr[(blockID % arr.length)] = blockID;
+    // this will overwrite the oldest blockId in the buffer if buffer is full
+    function setHash(uint256 blockId, bytes32 blockHash) payable public {
+        
+        arr[(blockId % arr.length)] = blockHash;
     }
 
-    // Gets the blockID at index blockID % 10
-    // Will return a valid blockID even for overwritten blocks
-    function getNumber(uint256 blockID) public view returns (uint256) {
-        return arr[(blockID % arr.length)];
+    // Gets the blockId at index blockID % 10
+    // Will return a valid blockId even for overwritten blocks
+    function getHash(uint256 blockId) public view returns (bytes32) {
+        return arr[(blockId % arr.length)];
     }
 }
