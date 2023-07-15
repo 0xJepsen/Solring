9ec1ee5d25a9771dbfd21a3bb71bab106c3dfa28
Philogy <philippe@dumo.net>
2023-07-15 17:18:09 +0200

    ♻️ Apply `forge fmt`

--- src/Solring.sol
+++ src/Solring.sol
@@ -3,15 +3,14 @@ pragma solidity ^0.8.13;
 
 // Ring Buffer in solidity for story blockIDs
 contract Solring {
-    // Size of buffer is 10 
+    // Size of buffer is 10
     bytes32[10] public arr;
 
     // Set blockId at index blockId % 10
-    // Since blocks increase in a linear fassion 
+    // Since blocks increase in a linear fassion
     // we can use the modulus operator to always set the index
     // this will overwrite the oldest blockId in the buffer if buffer is full
-    function setHash(uint256 blockId, bytes32 blockHash) payable public {
-        
+    function setHash(uint256 blockId, bytes32 blockHash) public payable {
         arr[(blockId % arr.length)] = blockHash;
     }
 
